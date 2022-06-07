6dd09de1d4158a8630e290e1251f89e0159db7f6
Denis Ubozhenko <d.ubozhenko@joinposter.com>
2022-06-07 14:25:06 +0300

    animation block

--- src/components/Block/index.tsx
+++ src/components/Block/index.tsx
@@ -8,8 +8,13 @@ interface BlockProps {
 }
 
 export const Block: React.FC<BlockProps> = ({block}) => {
+    const delay = block.x < 3 ? block.x / 10 : .2;
+    
 
     return (
-        <div className="cube"><span className="marker">{block.value}</span></div>
+        <div className="cube" style={{animationDelay: `${delay}s`}}>
+            <span className="marker">{block.value}</span>
+            <img src="./images/boom.svg" className="boom-effect" alt="Boom" />
+        </div>
     );
 }
